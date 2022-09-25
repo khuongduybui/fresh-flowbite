12c11c829fd16c61359c8eae989f30805d6779bf
Duy K. Bui <duy@buifamily.info>
2022-09-24 20:31:02 -0400

    release: 0.0.1-1.5.3

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { PluginRenderContext } from "$fresh/src/server/types.ts";
 
 export const flowbitePlugin = {
     name: "flowbite",
-    entrypoints: {"main": "https://raw.githubusercontent.com/khuongduybui/fresh-flowbite/main/plugin.ts"},
+    entrypoints: {"main": "https://raw.githubusercontent.com/khuongduybui/fresh-flowbite/0.0.1-1.5.3/plugin.ts"},
     render(ctx: PluginRenderContext) {
       ctx.render();
       return {
