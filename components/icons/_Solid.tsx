0814f9e12db2bdfaa993b5435ed930368779a39e
Duy K. Bui <duy@buifamily.info>
2022-10-04 15:15:09 -0400

    feat: add <SolidIcon/>

---
+++ components/icons/_Solid.tsx
@@ -0,0 +1,11 @@
+import { JSX } from "preact";
+
+export default function SolidIcon({ class: extraClass = "", ...props }: JSX.SVGAttributes<SVGSVGElement>) {
+  const commonClassNames = [
+    "w-4",
+    "h-4",
+  ];
+  const classNames = commonClassNames.concat([extraClass]);
+
+  return <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" {...props} class={classNames.join(" ")} />;
+}
